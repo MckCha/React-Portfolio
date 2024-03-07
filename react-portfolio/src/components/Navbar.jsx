192f107e58c87c6cedfed42481a2095f14f482e7
Mike Cha <mikecha321@gmail.com>
2024-03-06 19:05:01 -0800

    feat(navbar): Add logo

--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -2,6 +2,11 @@
 function Navbar() {
     return (
         <div className="nav">
+            <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/fcfc5b1c171b19c1544a3aed8c8ef1d78cbec5a912a536af441b2f4791461a2b?"
+                className="img"
+            />
             <div className="nav-name">Mike Cha</div>
             <div className="nav-buttons">
                 <a>About</a>
