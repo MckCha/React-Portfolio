612b57c506ba5a691bdfeb3ded93e1a44bb55b11
Mike Cha <mikecha321@gmail.com>
2024-03-09 21:53:17 -0800

    style(title): Adjust margin spacing

--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
                 src="https://cdn.builder.io/api/v1/image/assets/TEMP/fcfc5b1c171b19c1544a3aed8c8ef1d78cbec5a912a536af441b2f4791461a2b?"
                 className="img"
             />
-            <div className="nav-name">Mike Cha</div>
+            <div className="title">Mike Cha</div>
             <div className="nav-buttons">
                 <a>About</a>
                 <a>Projects</a>
