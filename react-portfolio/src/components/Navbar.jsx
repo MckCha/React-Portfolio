f18867c2ca1dd1310720c281f648af11c23e52a6
Mike Cha <mikecha321@gmail.com>
2024-03-10 15:45:58 -0700

    style(navbar): Update styling

--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -7,11 +7,11 @@ function Navbar() {
                 src="https://cdn.builder.io/api/v1/image/assets/TEMP/fcfc5b1c171b19c1544a3aed8c8ef1d78cbec5a912a536af441b2f4791461a2b?"
                 className="img"
             />
-            <div className="title">Mike Cha</div>
-            <div className="nav-buttons">
-                <a>About</a>
-                <a>Projects</a>
-                <a>Info</a>
+            <div className="title mainTitle">Mike Cha</div>
+            <div className="navButtons">
+                <div className="navButton">About</div>
+                <div className="navButton">Project</div>
+                <div className="navButton">Info</div>
             </div>
         </div>
     )
