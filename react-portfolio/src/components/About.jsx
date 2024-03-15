1d60f8641cf30b1487e91f5df019767acd0d6c7d
Mike Cha <mikecha321@gmail.com>
2024-03-14 21:52:52 -0700

    style(): Update project design
    - Changed fonts
    - Added padding

--- react-portfolio/src/components/About.jsx
+++ react-portfolio/src/components/About.jsx
@@ -3,18 +3,12 @@ function About() {
     return (
         
         <div className="aboutContainer">
-            <div className="aboutRow" >
+            <div className="aboutRow">
                 <div className="aboutDescription">
                     <div className="title">About</div>
-                    Hi, I’m Mike, a junior developer dedicated to <br />
-                    continuous growth and learning in the world<br />
-                    of software development. Graduating with a <br />
-                    solid foundation in Computer Science, I am now <br />
-                    applying my skills to real-world applications. <br />
-                    In addition, I stay engaged by working on projects <br />
-                    and LeetCode to reinforce my knowledge. <br />
-                    Eager to accelerate my growth, I am always <br />
-                    seeking opportunities to enhance my skills.
+                    Hi, I'm Mike, a junior developer with a passion for learning and applying software <br />
+                    development skills. Armed with a Computer Science background. I'm actively <br /> engaged 
+                    in real-world projects and challenges,continuously seeking opportunities to grow."
                 </div>
                 <div className="profilePicture"></div>
             </div>
