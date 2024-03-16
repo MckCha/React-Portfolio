be13b37bb8eae495fa80b5f3604ae17ac4dfe5f5
Mike Cha <mikecha321@gmail.com>
2024-03-15 21:49:18 -0700

    style(): fix typography design

--- react-portfolio/src/components/About.jsx
+++ react-portfolio/src/components/About.jsx
@@ -5,10 +5,12 @@ function About() {
         <div className="aboutContainer">
             <div className="aboutRow">
                 <div className="aboutDescription">
-                    <div className="title">About</div>
-                    Hi, I'm Mike, a junior developer with a passion for learning and applying software <br />
-                    development skills. Armed with a Computer Science background. I'm actively <br /> engaged 
-                    in real-world projects and challenges,continuously seeking opportunities to grow."
+                    <div className="title" style={{textAlign: 'left'}}>About</div>
+                    <div className="aboutText">
+                        Hi, I'm Mike, a junior developer with a passion for learning and applying software
+                        development skills. Armed with a Computer Science background. I'm actively engaged 
+                        in real-world projects and challenges,continuously seeking opportunities to grow."
+                    </div>
                 </div>
                 <div className="profilePicture"></div>
             </div>
