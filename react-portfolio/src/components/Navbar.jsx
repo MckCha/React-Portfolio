6f366a9f9334867757a27ab61b41bf3edd110cb4
Mike Cha <mikecha321@gmail.com>
2024-03-30 21:11:51 -0700

    feature(navButton): add scroll down on click

--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -1,12 +1,21 @@
+import About from "./About";
 
 function Navbar() {
+    const scrollToSection = (className) => {
+        const section = document.querySelector(`.${className}`);
+        if (section) {
+            const scrollPosition = section.offsetTop - (window.innerHeight / 2) + (section.offsetHeight / 2);
+            window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
+        }
+    };
+    
+
     return (
         <div className="navContainer">
             <div className="title name">Mike Cha</div>
             <div className="navButtons">
-                <div className="navButton">About</div>
-                <div className="navButton">Project</div>
-                <div className="navButton">Info</div>
+                <div className="navButton" onClick={() => scrollToSection('aboutContainer')}>About</div>
+                <div className="navButton" onClick={() => scrollToSection('projectContainer')}>Projects</div>
             </div>
         </div>
     )
